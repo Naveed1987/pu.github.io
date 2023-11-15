54223ce3790dd0f56a0ef9e1e259393a99c03138
Naveed1987 <mna909@gmail.com>
2023-11-15 13:02:28 +0500

    pu

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function first(){
+    document.getElementById("pu-img").src= "./images/2.jpg"
+
+}
+function second(){
+    document.getElementById("pu-img").src= "./images/3.jpg"
+
+}
+function third(){
+    document.getElementById("pu-img").src= "./images/4.jpg"
+
+}
+function four(){
+    document.getElementById("pu-img").src= "./images/1.jpg"
+
+}
+
+setInterval(first, 4000)
+setInterval(second, 8000)
+setInterval(third, 12000)
+setInterval(four, 16000)
